0dbdb4a5cb8a98a1280f6e4ddd2d07de551dd3bb
aniketnakhate <aniketpn123@gmail.co>
2026-02-05 01:09:48 +0530

    array file

---
+++ Arrays.js
@@ -0,0 +1,25 @@
+let arr = new Array();
+
+let arr2 =[];
+
+let fruits =["apple","orange","plum"];
+
+fruits[1] = "pear";
+fruits.length;
+fruits[0];
+
+let arr3 = ["aniket",'A',335,{name:'ani'},true, function () {console.log('hello');
+}]
+fruits.at(-1);
+
+let no =[1,2,3,4,5,6,7,8,9,10];
+
+no.push(11,34,66,88);
+no.pop();
+no.shift();
+no.unshift('f','55','aniekt','77');
+console.log(no);
+
+
+
+
