fcee4b217569b5e786016e21349cf0716f9fb02f
aniketnakhate <aniketpn123@gmail.com>
2026-03-07 21:15:58 +0530

    error handling

--- ErrorHandling.js
+++ ErrorHandling.js
@@ -95,9 +95,128 @@ try{
         throw new SyntaxError("incomplete data");
     }
     console.log(user.name);
-    
 }
 catch(err){
     console.log("JSon error:" + err.msg);
-    
 }
+
+// rethrowing
+
+try{
+    user = JSON.parse(json);
+}
+catch(err){
+    console.log('json error ' + err);
+}
+
+json ='{"age" :30}';
+
+try{
+    let user = JSON.parse(json);
+    if(!user.name){
+        throw new SyntaxError("no name");
+    }
+
+    lalal();
+
+    console.log(user.name);
+}
+catch(err){
+    if(err instanceof SyntaxError){
+        console.log("json error" + err.msg);
+    }
+    else{
+        throw err;
+    }
+}
+
+function readData(){
+    let json = '{"age : 30}';
+
+    try{
+        lalal();
+    }
+    catch(err){
+        if(!err instanceof SyntaxError)
+        {
+            throw err;
+        }
+    }
+}
+
+try{
+    readData();
+}
+catch(err){
+    console.log("external catch "+err);
+}
+
+
+// try cath finally 
+
+// finally always run if tehre is try no error or catch errros
+
+try{
+console.log();
+}
+catch(err){
+    console.log(err); 
+}
+finally{
+    console.log('execute always');
+}
+
+let num = +prompt('enetr a no ', 35)
+
+let diff,result;
+
+function fib(n){
+    if(n< 0 || Math.trunc(n) != n){
+        throw new Error('must not be -ve also an int');
+    }
+    return n <= 1 ? n : fib(n-1) + fib(n-2);
+}
+
+let start = Date.now();
+
+try{
+    result = fib(num);
+}
+catch(err){
+    result =0 ;
+}
+finally{
+    diff =Date.now() - start;
+}
+
+console.log(result || "error occured");
+
+console.log( `execution took ${diff}ms`);
+
+
+// Gloabl catch
+
+window.onerror =function(message,url, line, col,error){
+
+};
+
+window.onerror =function(mesage,url, line, col, error){
+    console.log(`${mesage} \n at ${line} : ${col} of ${url}`);
+}
+
+function readData(){
+    badFunc();
+}
+
+readData();
+
+// summary 
+
+
+// try ... catch  always handle runtime erorrs
+
+
+
+
+//   custom errors , extending errors
+
